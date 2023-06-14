9b31ebe8cc020f430e541ebf3c83fbe863e6fe68
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-14 22:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function executor(resolve, reject) {
+	setTimeout(()=> {
+		resolve("Hello, world!")
+	},1000);
+}
+var promise = new Promise(executor);
+function printhello(data) {
+	const out = document.getElementById("output");
+	out.innerText = data;
+}
+promise.then(printhello);
